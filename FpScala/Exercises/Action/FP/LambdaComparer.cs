0689304b6f2c76cebcbade8f900cbb21b54260a5
Rastislav Svoboda <rastislav.svoboda@gmail.com>
2024-02-20 07:47:49 +0100

    add utility classes Option, LambdaComparer

---
+++ FpScala/Exercises/Action/FP/LambdaComparer.cs
@@ -0,0 +1,14 @@
+﻿namespace FpScala.Exercises.Action.FP;
+
+public class LambdaComparer<T> : IComparer<T>
+{
+    private readonly Func<T, T, int> _compFunc;
+
+    public LambdaComparer(Func<T, T, int> compFunc)
+    {
+        _compFunc = compFunc;
+    }
+
+    public int Compare(T? x, T? y) =>
+        _compFunc(x, y);
+}
